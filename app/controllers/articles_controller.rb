13efc69a96151fbb732788c02a6d3c48af11a802
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-29 17:50:21 +0100

    split out .permit to private method so it can be reused in the controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,11 +4,17 @@ class ArticlesController < ApplicationController
   def new; end
 
   def create
-    @articles = Article.new(params.require(:article).permit(:title, :text))
+    @articles = Article.new(article_params)
     # Article.new is referring to app/models/article.rb
     # info in article params are auto mapped to the correct tables
     # for safety we need to tell ruby what information is permitted before it can be accepted
     @article.save
     redirect_to @article
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
