3849b6d2a3752ba50d0cb8b4c1ea7a710f8768c8
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-29 17:08:52 +0100

    looked into the use of resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
+  resources :articles
+  # created CRUD RESTful routes for articles/...
+
   root 'welcome#index'
   # tells rails to map requests to the root of the application to the welcome controllers index action
 end
