d0bae80fb3a114ab11c029e8ebc238150779ca34
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-29 17:49:01 +0100

    added logic so info passed in via form can be used and stored in the db

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,11 @@ class ArticlesController < ApplicationController
   def new; end
 
   def create
-    render plain: params[:article].inspect
+    @articles = Article.new(params.require(:article).permit(:title, :text))
+    # Article.new is referring to app/models/article.rb
+    # info in article params are auto mapped to the correct tables
+    # for safety we need to tell ruby what information is permitted before it can be accepted
+    @article.save
+    redirect_to @article
   end
 end
