8de84e55c23f14b4e7faee4d9d19124a660e07f5
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-29 18:03:40 +0100

    able to see posted article on show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
+  def show
+    @article = Article.find(params[:id])
+  end
+  # Article.find is used to find the article we want, using the passed in ID params from the url
+
   def new; end
 
   def create
-    @articles = Article.new(article_params)
+    @article = Article.new(article_params)
     # Article.new is referring to app/models/article.rb
     # info in article params are auto mapped to the correct tables
     # for safety we need to tell ruby what information is permitted before it can be accepted
@@ -18,3 +23,6 @@ class ArticlesController < ApplicationController
     params.require(:article).permit(:title, :text)
   end
 end
+
+# A frequent practice is to place the standard CRUD actions in each controller in the following order:
+# index, show, new, edit, create, update and destroy.
