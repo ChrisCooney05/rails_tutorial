564044bcc2013a21f512edbd2a8c62627d25b418
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-30 08:28:21 +0100

    logic added it Comments#create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
+# frozen_string_literal: true
+
 class CommentsController < ApplicationController
+  def create
+    @article = Article.find(params[:id])
+    @comment = @article.comments.create(comment_params)
+    redirect to article_path(@article)
+  end
+
+  # must first call out the article as all comments are linked to them
+  # by calling the create method on the comments linked to article,
+  # we automatically set up the relation between them
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
 end
